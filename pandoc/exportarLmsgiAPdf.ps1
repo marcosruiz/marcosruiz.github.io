eea488ce48b9d3f4009a57e22b8d52072999c05d
Marcos Ruiz <marcosruizgarcia@gmail.com>
2024-04-09 20:48:45 +0200

    wip

--- pandoc/exportarLmsgiAPdf.ps1
+++ pandoc/exportarLmsgiAPdf.ps1
@@ -1,10 +1,12 @@
 $githubProjectFolder = "C:\Users\marco\Documents\GitHub\marcosruiz.github.io"
 $templateFolder = "C:\Users\marco\Documents\MEGA\CURSO 23 24\Plantillas\Eisvogel"
 
+$githubProjectFolder = "D:\GitHub\marcosruiz.github.io"
+$templateFolder = "C:\Users\marco\MEGA\CURSO 23 24\Plantillas\Eisvogel"
+
 $routes = "css", "dtd", "espacios-de-nombres-xml", "html", "introduccion-lenguajes-marcas", "markdown", "practica-css-codepip", "practica-css-freecodecamp", "practica-css-w3schools", "practica-html", "practica-html-freecodecamp", "practica-html-w3schools", "representacion-informacion", "sindicacion-contenidos", "sistemas-gestion-empresarial", "tarea-calculadora-html-css", "tarea-dtd", "tarea-formato-svg", "tarea-introduccion-odoo", "tarea-introduccion-xpath", "tarea-introduccion-xml", "tarea-introduccion-xquery", "tarea-rss", "tarea-xsd", "tarea-xsl", "xml", "xpath", "xquery", "xsl", "xslfo", "xslt"
 
-$routes = "sistemas-gestion-empresarial"
-
+$routes = "xslt"
 
 # Iterar sobre cada patrón de archivo
 foreach ($route in $routes) {
